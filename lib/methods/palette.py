50c39c4ea429656ab5dcc653cd8a05899cc30ea1
Victor Johnson <vicjohnson1213@gmail.com>
2019-01-08 01:49:40 -0600

    Updated the palettes method to use a weighted euclidean distance

--- lib/methods/palette.py
+++ lib/methods/palette.py
@@ -8,11 +8,14 @@ def generateGetColor(paletteName):
     def getColor(color):
         """ Generates a color by finding the most similar color from a specified color palette """
 
-        def euclidean_difference(c1, c2):
+        def euclideanDifference(c1, c2):
             return math.sqrt( ((c2[0] - c1[0]) ** 2) + ((c2[1] - c1[1]) ** 2) + ((c2[2] - c1[2]) ** 2) )
 
+        def weightedEuclideanDifference(c1, c2):
+            return math.sqrt( (((c2[0] - c1[0]) * 0.3) ** 2) + (((c2[1] - c1[1])*0.59) ** 2) + (((c2[2] - c1[2])*0.11) ** 2) )
+
         def get_diffs(c):
-            return (euclidean_difference(c, color), c)
+            return (weightedEuclideanDifference(c, color), c)
 
         if paletteName in palettes:
             palette = palettes[paletteName]
